2e16647882c46ecb0da9f0251be73771c8dc548c
GlebVoronin <47082791+GlebVoronin@users.noreply.github.com>
2020-05-20 03:27:22 +0300

    add hiragana model

---
+++ data/models/hiragana.py
@@ -0,0 +1,10 @@
+import sqlalchemy
+from data.db_session import SqlAlchemyBase
+
+
+class Hiragana(SqlAlchemyBase):
+    __tablename__ = 'hiragana'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False, index=True)
+    reading = sqlalchemy.Column(sqlalchemy.String, nullable=False, index=True)
+    way_to_sound = sqlalchemy.Column(sqlalchemy.String, nullable=True, index=True)
