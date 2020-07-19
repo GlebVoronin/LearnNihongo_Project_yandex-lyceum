d5df53da5a57cbdb9f511360284444dbad43a0c3
GlebVoronin <47082791+GlebVoronin@users.noreply.github.com>
2020-07-19 18:07:54 +0300

    bug fix

--- data/register.py
+++ data/register.py
@@ -2,75 +2,132 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QMainWindow, QPushButton, QLabel, QLineEdit)
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from data import Nihongo
 from data import db_session
-from data.style import FONT_20, FONT_14
+from data.consts import *
 from data.models.users import User
+from data.style import *
+
+
 ##############################################доделать
 
 class LoginRegisterMenu(QMainWindow):
     def __init__(self, parent):
         super().__init__(parent, Qt.Window)
-        db_session.global_init(f'db/{Nihongo.DB_FILE_NAME}')
+        self.resize(700, 450)
+        self.setWindowTitle("Программа для помощи в изучении японского языка")
+        db_session.global_init(f'db/{DB_FILE_NAME}')
+        self.ui_list = []
         self.login_menu()
 
+    def disable_ui(self):
+        for ui_item in self.ui_list:
+            ui_item.setParent(None)
+            del ui_item
+
+    def set_style_and_show_all(self):
+        for ui_object in self.children():
+            if hasattr(ui_object, 'setStyleSheet'):
+                if isinstance(ui_object, QPushButton):
+                    set_color(ui_object, MAIN_COLORS['main_button'])
+                else:
+                    set_color(ui_object, MAIN_COLORS['menu'])
+            if hasattr(ui_object, 'show'):
+                ui_object.show()
+
     def login(self, ui, register=False):
+        login = ui['login'].text()
+        password = ui['password'].text()
         session = db_session.create_session()
-        login = ui['login_line'].text()
-        password = ui['password_line'].text()
-        if not register:
+        if register:
+            repeat_password = ui['repeat'].text()
+            if password != repeat_password:
+                ui['info'].setText('Пароли должны совпадать!')
+            else:
+                user = User(
+                    login=login,
+                    password_hash=generate_password_hash(password)
+                )
+                session.add(user)
+                session.commit()
+                self.parent().current_user = user
+                self.destroy()
+        else:
             user = session.query(User).filter(
                 User.login == login,
                 check_password_hash(User.password_hash, password)
             ).first()
             if user:
-                self.current_user = user
-                self.start_learn()
+                self.parent().current_user = user
+                self.destroy()
             else:
                 ui['info'].setText('Неверный логин или пароль!')
-        else:
-            user = User(
-                login=login,
-                password_hash=generate_password_hash(password)
-            )
-            session.add(user)
-            session.commit()
-            self.current_user = user
-            self.start_learn()
 
-    def login_menu(self, register=False):
+    def register_menu(self):
+        self.disable_ui()
+        info_label = QLabel('Введите свой логин и пароль', self)
+        info_label.setGeometry(50, 40, 600, 40)
+        info_label.setFont(FONT_14)
+        info_label.setAlignment(Qt.AlignCenter)
+
+        info_login_label = QLabel('Логин: ', self)
+        info_login_label.setGeometry(10, 100, 130, 60)
+        info_login_label.setFont(FONT_14)
+        login_line = QLineEdit(self)
+        login_line.setGeometry(150, 100, 500, 60)
+
+        info_password_label = QLabel('Пароль: ', self)
+        info_password_label.setGeometry(10, 190, 130, 60)
+        info_password_label.setFont(FONT_14)
+        password_line = QLineEdit(self)
+        password_line.setGeometry(150, 190, 500, 60)
+
+        info_repeat_password_label = QLabel('Повторите\nпароль: ', self)
+        info_repeat_password_label.setGeometry(10, 280, 130, 60)
+        info_repeat_password_label.setFont(FONT_14)
+        repeat_password_line = QLineEdit(self)
+        repeat_password_line.setGeometry(150, 280, 500, 60)
+
+        confirm_button = QPushButton('Подтвердить', self)
+        confirm_button.setGeometry(50, 380, 600, 60)
+        confirm_button.setFont(FONT_20)
+        ui = {'login': login_line,
+              'password': password_line,
+              'repeat': repeat_password_line,
+              'info': info_label}
+        confirm_button.clicked.connect(lambda: self.login(ui, register=True))
+        self.ui_list.extend(ui.values())
+        self.ui_list.extend([info_login_label, info_password_label,
+                             info_repeat_password_label, confirm_button])
+        self.set_style_and_show_all()
+
+    def login_menu(self):
+        self.disable_ui()
         info_label = QLabel('Введите свой логин и пароль', self)
         info_label.setGeometry(50, 40, 600, 40)
         info_label.setFont(FONT_14)
         info_label.setAlignment(Qt.AlignCenter)
         info_login_label = QLabel('Логин: ', self)
-        info_login_label.setGeometry(10, 100, 80, 60)
+        info_login_label.setGeometry(10, 100, 130, 60)
         info_login_label.setFont(FONT_14)
         info_password_label = QLabel('Пароль: ', self)
-        info_password_label.setGeometry(10, 210, 80, 60)
+        info_password_label.setGeometry(10, 210, 130, 60)
         info_password_label.setFont(FONT_14)
         login_line = QLineEdit(self)
-        login_line.setGeometry(100, 100, 500, 60)
+        login_line.setGeometry(150, 100, 500, 60)
         password_line = QLineEdit(self)
-        password_line.setGeometry(100, 210, 500, 60)
+        password_line.setGeometry(150, 210, 500, 60)
         confirm_button = QPushButton('Подтвердить', self)
         confirm_button.setGeometry(50, 300, 600, 60)
         confirm_button.setFont(FONT_20)
-        ui = {'info': info_label,
-              'login_label': info_login_label,
-              'password_label': info_password_label,
-              'login_line': login_line,
-              'password_line': password_line,
-              'confirm': confirm_button}
-        if not register:
-            register_button = QPushButton('Зарегистрироваться', self)
-            register_button.setGeometry(50, 380, 600, 60)
-            register_button.setFont(FONT_20)
-            register_button.clicked.connect(lambda: self.login_menu(register=True))
-            ui['register'] = register_button
-        self.disable_ui(without=ui.values())
+        register_button = QPushButton('Зарегистрироваться', self)
+        register_button.setGeometry(50, 380, 600, 60)
+        register_button.setFont(FONT_20)
+        register_button.clicked.connect(self.register_menu)
+        ui = {'login': login_line,
+              'password': password_line,
+              'info': info_label}
+        confirm_button.clicked.connect(lambda: self.login(ui))
         self.ui_list.extend(ui.values())
-        if not register:
-            confirm_button.clicked.connect(lambda: self.login(ui))
-        else:
-            confirm_button.clicked.connect(lambda: self.login(ui, register=True))
+        self.ui_list.extend([info_login_label, info_password_label,
+                             register_button, confirm_button])
+        self.set_style_and_show_all()
